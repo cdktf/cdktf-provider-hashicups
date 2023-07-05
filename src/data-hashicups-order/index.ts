ace96fdb3481e795499d900e4136095c54ec5016
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-07-05 08:51:12 +0000

    chore(deps): Updated

--- src/data-hashicups-order/index.ts
+++ src/data-hashicups-order/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/hashicorp/hashicups/0.3.1/docs/data-sources/order
 // generated from terraform resource schema
 
